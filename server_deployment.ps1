6ac753a1d74a5e7bb5172ecfec85c1743b8b340d
rcaoagdan <rcaoagdan@icloud.com>
2021-06-11 21:25:07 -0700

    test

--- server_deployment.ps1
+++ server_deployment.ps1
@@ -24,16 +24,15 @@ function mainmenu {
     Write-Output "5. Mange Users"
     Write-Output "6. Install RADIUS"
     $userinput = Read-Host "Option"
-
     if($userinput -eq 1){
         Write-Output "Installing Active Directory `n"
         installAD
     }elseif ($userinput -eq 2) {
         Write-Output " "
-        setStaticIP
+        printIPset
     }elseif ($userinput -eq 3) {
         Write-Output " "
-        manageDNS
+        checkDNSstat
     }elseif ($userinput -eq 4) {
         Write-Output " "
         manageOU
@@ -56,11 +55,9 @@ function mainmenu {
 # Renames Server   
 ##############################################################################
 function installAD {
-
     Get-WindowsFeature -Name AD-Domain-Services | Install-WindowsFeature
     Import-Module ADDSDeployment
     Install-ADDSForest -DomainName GlobeXPrimary.local -DomainNetbiosName GlobeXPrimary 
-
 }
 
 
@@ -68,13 +65,40 @@ function installAD {
 # Assign Windows Server Static IPv4 Address   
 # Insert IP addressess where ""                                                                     
 ##############################################################################
-function setStaticIP{
 
-    Write-Output "Manage IP `n"
+function printIPset {
+    Write-Output " The Current IP settings"
+    Get-NetIPAddress -AddressFamily IPv4
+    manageIP
+}
+function manageIP {
+    $ipOPT = Read-Host "Shall set a Static IP address Y/N?"
+    if($ipOPT  -eq "Y"){
+        setStaticIP
+    }elseif ($ipOPT  -eq "y") {
+        setStaticIP
+    }elseif($ipOPT  -eq "N"){
+        Write-Output "Returning to Main Menu"
+        Write-Output " "
+        mainmenu
+    }elseif ($ipOPT  -eq "n") {
+        Write-Output "Returning to Main Menu"
+        Write-Output " "
+        mainmenu
+    }else{
+        Write-Output "Incorrect Selection `n"
+        manageIP
+    }
+}
+function setStaticIP{
     Write-Output "Enter Static IP:"
     $staticIPadd = Read-Host " "
     Write-Output "Enter Gateway:"
     $gatewayIP = Read-Host " "
+    confrimIP
+}
+
+function confrimIP {
     Write-Output " The Static IP is : $staticIPadd and the Gateway : $gatewayIP"
     $statConfirm = Read-Host "Confirm Y/N | M for main menu"
     if($statConfirm -eq "Y"){
@@ -91,47 +115,45 @@ function setStaticIP{
         mainmenu
     }else {
         Write-Output "Incorrect Selection `n"
-        setStaticIP
+        confrimIP
     }
-    
 }
-
 
 ##############################################################################
 # Assign Window Server a DNS          
 # set ip address between "" use a , for two addresses                                                            
 ##############################################################################
-function manageDNS {
-    
+function checkDNSstat {
     Write-Output "Checking if DNS Role is currently Installed"
     Get-WindowsFeature | where {($_.name -like "DNS")}
-    $dnsopt1 = Read-Host "Does DNS need to be installed Y/N | M for main menu"
+    manageDNS
+}
+function manageDNS {
+    $dnsopt1 = Read-Host "Does DNS need to be installed Y/N "
     if($dnsopt1 -eq "Y"){
         Install-WindowsFeature DNS -IncludeManagementTools
-        Write-Output " "
-        manageDNS
+        Write-Output "DNS Server Installed `n "
+        mainmenu
     }elseif ($dnsopt1 -eq "y"){
         Install-WindowsFeature DNS -IncludeManagementTools
-        Write-Output " "
-        manageDNS
+        Write-Output " DNS Server Installed `n"
+        mainmenu
     }elseif ($dnsopt1 -eq "N"){
-        manage2DNS
+        currentDNS
     }elseif ($dnsopt1 -eq "n") {
-        manage2DNS
-    }elseif ($dnsopt1 -eq "M") {
-        mainmenu
-    }elseif ($dnsopt1 -eq "m") {
-        mainmenu
+       currentDNS     
     }else {
-        Write-Output "Incorrect Selection `n"
-        dnsMainreturn
+        Write-Output "Incorrect Selection"
+        manageDNS
     }
 }
 
-function manage2DNS{
-
+function currentDNS {
     Write-Output "Current DNS Settings "
     Get-DnsClientServerAddress -AddressFamily IPv4 -InterfaceIndex(Get-NetAdapter).InterfaceIndex
+    manage2DNS
+}
+function manage2DNS{
     $dnsopt2 = Read-Host "Shall We Assign a DNS Y/N"
     if($dnsopt2 -eq "Y"){
         setDNS
@@ -139,15 +161,13 @@ function manage2DNS{
         setDNS
     }elseif($dnsopt2 -eq "N"){
         Write-Output "Returning to Main Menu"
-        Write-Output " "
         mainmenu
     }elseif ($dnsopt2 -eq "n") {
         Write-Output "Returning to Main Menu"
-        Write-Output " "
         mainmenu
     }else{
         Write-Output "Incorrect Selection `n"
-        dnsMainreturn
+        manage2DNS
     }
 }
         
@@ -158,6 +178,9 @@ function setDNS{
     $servIP = Read-Host " "
     Write-Output "Please Enter Secondary IP Address for DNS"
     $servIP2 = Read-Host " "
+    confirmDNS
+}
+function confirmDNS {
     Write-Output "$servIP & $servIP2 will be set for DNS"
     $dnsopt3 = Read-Host "Confirm Y/N"
     if ($dnsopt3 -eq "Y") {
@@ -170,26 +193,10 @@ function setDNS{
         setDNS
     }else{
         Write-Output "Incorrect Selection `n"
-        dnsMainreturn
+        confirmDNS
+    }
 }
 
-function dnsMainreturn {
-    $dnsReturn = Read-Host "Do you want to return to main menu Y/N?"
-    if ($dnsReturn -eq "Y") {
-        mainmenu
-    }elseif ($dnsReturn -eq "y") {
-        mainmenu
-    }elseif ($dnsReturn -eq "N") {
-        manageDNS
-    }elseif ($dnsReturn -eq "n") {
-        manageDNS
-    }else {
-        Write-Output "Incorrect selection `n"
-        dnsMainreturn
-    }
-    
-}
-}
 ##############################################################################
 #  Manage OUs                                                                          
 ##############################################################################
